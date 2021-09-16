7a5c4566254935e9777e7221ed04375e6d04d03c
MMercer8989 <46968747+MMercer8989@users.noreply.github.com>
2021-09-15 20:26:25 -0400

    Added placeholder buttons
    I have added buttons to the window which can later be used to power the device off and launch a window to manage device connections. Additionally, I have implemented a slider which can be used to adjust the temperature of the blanket

--- Raspberry_Pi/main.py
+++ Raspberry_Pi/main.py
@@ -10,10 +10,20 @@ class GUI(tk.Frame):
         self.draw_ui()
 
     def draw_ui(self):
-        self.master.geometry("600x300")
+        self.master.geometry("300x500")
         self.master.title("Smart Blanket")
         self.master.resizable(False, False)
-         
+        self.button_frame = tk.Frame(self.master, borderwidth=4, relief=GROOVE, height=500, width=300)
+        self.draw_buttons()
+        self.button_frame.pack(side=tk.TOP)
+    
+    def draw_buttons(self):
+        self.power_button = tk.Button(self.button_frame, text="Power", fg='red', height=2, width=20)
+        self.wifi_button = tk.Button(self.button_frame, text="Wifi", fg='blue', height=2, width=20)
+        self.temp_scale = tk.Scale(self.master, from_=0, to=140, tickinterval=1, length=450)
+        self.power_button.pack(side=tk.RIGHT)
+        self.wifi_button.pack(side=tk.LEFT)
+        self.temp_scale.pack(side=tk.RIGHT)
 
 root = tk.Tk()
 interface = GUI(master=root)
