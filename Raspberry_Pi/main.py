0f38f12672343cc3d05c5f923b21f9a21f5a9387
MMercer8989 <46968747+MMercer8989@users.noreply.github.com>
2021-09-21 18:41:26 -0400

    Added Temperature Display
    I have implemented a label on the GUI which can be used to display the current temperature of the blanket. To go along with this change, I have also implemented a setter for the label text and a getter for the value of the temperature slider.

--- Raspberry_Pi/main.py
+++ Raspberry_Pi/main.py
@@ -7,6 +7,8 @@ class GUI(tk.Frame):
         super().__init__(master)
         self.master = master
         self.pack() 
+        self.requestedTemp = tk.DoubleVar() #declare a variable that will be set by the slider
+        self.displayTemp = tk.StringVar() #declare a variable to hold the current temperature as a string
         self.draw_ui()
 
     def draw_ui(self): #define window dimensions and properties
@@ -15,15 +17,29 @@ class GUI(tk.Frame):
         self.master.resizable(False, False)
         self.button_frame = tk.Frame(self.master, borderwidth=4, relief=GROOVE, height=500, width=300) #define a container for buttons
         self.draw_buttons()
+        self.draw_TempFrame()
         self.button_frame.pack(side=tk.TOP) #make sure the frame is included at the top of the window
     
     def draw_buttons(self): #define the buttons that will populate the frame and window
         self.power_button = tk.Button(self.button_frame, text="Power", fg='red', height=2, width=20)
         self.wifi_button = tk.Button(self.button_frame, text="Wifi", fg='blue', height=2, width=20)
-        self.temp_scale = tk.Scale(self.master, from_=68, to=140, tickinterval=1, length=450)
+        self.temp_scale = tk.Scale(self.master, from_=68, to=140, tickinterval=1, length=450, variable=self.requestedTemp)
         self.power_button.pack(side=tk.RIGHT) #start packing the buttons in their containers
         self.wifi_button.pack(side=tk.LEFT)
         self.temp_scale.pack(side=tk.RIGHT)
+
+    def draw_TempFrame(self): #draw the label that will display the current temperature
+        self.TempFrame = tk.Label(self.master, height=5, width=20, textvariable=self.displayTemp, borderwidth=4, relief=GROOVE)
+        self.TempFrame.pack(side=tk.BOTTOM)
+    
+    def set_displayTemp(self, s): #set the temperature being displayed in the text field
+       self.displayTemp.set(s) #change the value of curTemp to s
+       #this function could allow us to fetch a temp reading from the sensors and display it.
+       #For a good example of how the text field can be manipulated, try changing the variable used
+       #in TempFrame from 'displayTemp' to 'requestedTemp' and observe.
+        
+    def get_requestedTemp(self):
+        return self.requestedTemp
 
 root = tk.Tk()
 interface = GUI(master=root)
