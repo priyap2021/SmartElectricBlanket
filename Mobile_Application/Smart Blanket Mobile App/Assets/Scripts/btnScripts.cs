63ca3c989f450c55aa3d925e74a5b9b9ee4dcd27
Seth Sarchet <smspeed99@hotmail.com>
2021-09-23 20:35:37 -0400

    Got the temp value to correspond with the slider
    I got a text value to correspond with where the slider is at. I had to use a UI Text - TextMeshPro object which made me have to install a bunch of packages to use it. That's why there are so many packages being pushed.

--- Mobile_Application/Smart Blanket Mobile App/Assets/Scripts/btnScripts.cs
+++ Mobile_Application/Smart Blanket Mobile App/Assets/Scripts/btnScripts.cs
@@ -7,10 +7,10 @@ public class btnScripts : MonoBehaviour
 {
 
     public Text label;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
