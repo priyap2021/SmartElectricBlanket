85e135417cae137d9881940b3b196db75d78e585
Seth Sarchet <smspeed99@hotmail.com>
2021-09-30 08:43:39 -0400

    Added a getter for currentTemp
    Added a getter so that we can get the current temperature.

--- Mobile_Application/Smart Blanket Mobile App/Assets/Scripts/sliderScript.cs
+++ Mobile_Application/Smart Blanket Mobile App/Assets/Scripts/sliderScript.cs
@@ -9,14 +9,23 @@ public class sliderScript : MonoBehaviour
     
     [SerializeField] private Slider _slider;
 
-    [SerializeField] private TextMeshProUGUI _sliderText;
+    [SerializeField] public TextMeshProUGUI _sliderText;
+
+    [SerializeField] public TextMeshProUGUI currentTemp;
     
     // Start is called before the first frame update
     void Start()
     {
         _slider.onValueChanged.AddListener((v) => {
             _sliderText.text = v.ToString("0°C");
+
+            currentTemp = getCurrentTemp();
+            Debug.Log("Current temp is " + currentTemp.text);
         });
+    }
+    // get the current value of the temperature
+    TextMeshProUGUI getCurrentTemp(){
+        return _sliderText;
     }
 
     // Update is called once per frame
