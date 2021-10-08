c57447cc352531af47180e4352e13741b7cdc7ca
MMercer8989 <46968747+MMercer8989@users.noreply.github.com>
2021-10-07 23:53:01 -0400

    Update Display.py and Implement arguments for connected devices
    In this commit, I have cleaned up some of the code in this script and have included arguments to 'draw_labels()' so that the labels, current temperature, and device connection status can all be printed at once.

--- Raspberry_Pi/Display.py
+++ Raspberry_Pi/Display.py
@@ -3,7 +3,7 @@ import board
 import busio as io
 import adafruit_ssd1306
 
-class On_Display():
+class On_Display:
     def __init__(self): #define the class constructor (master will usually be an instance of tk.Tk())
         #create an instance of the ssd1306 I2C driver
         self.oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
@@ -11,17 +11,20 @@ class On_Display():
         self.i2c = io.I2C(board.SCL, board.SDA)
         self.draw_labels() #make sure the labels are drawn
             
-    def draw_labels():
+    def draw_labels(newTemp, conDevice):
+        
         self.oled.fill(0) #clear screen and then write the text, more labels can be added if needed (not much though)
         self.oled.text('Temp: ', 0, 0)
         self.oled.text('Device: ', 0, 20)
+        
+        if newTemp is not None: #make sure newTemp actually exists before trying to write it
+            self.oled.text(newTemp, 15, 0)
+        else:
+            self.oled.text('No Temp', 15, 0)    
+                    
+        if conDevice is not None: #make sure conDevice actually exists before trying to write it
+            self.oled.text(conDevice, 15, 20)
+        else:
+            self.oled.text('No Device', 15, 20)   
+                    
         self.oled.show() #show the text that was just drawn, nuff said
-        
-    def update_temp(newTemp):
-        self.draw_labels() #make sure screen is refreshed and labels are redrawn, don't want overlapping temperatures
-        self.oled.text(newTemp, 7, 0) #draw the new temperature reading, make sure this is a string when it's passed in
-        self.oled.show() 
-        
-            
-
-
