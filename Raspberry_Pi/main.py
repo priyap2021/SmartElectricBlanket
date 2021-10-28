a543db3a9e638487d3d20bd8786c977c654f26bd
MMercer8989 <46968747+MMercer8989@users.noreply.github.com>
2021-10-27 21:17:33 -0400

    Correct Minor Mistakes in 'mainloop.py'
    Just a commit to correct a few more minor mistakes; I must have been tired when I first wrote this code. At any rate, this should be the last fix needed before testing it on real hardware.

--- Raspberry_Pi/main.py
+++ Raspberry_Pi/main.py
@@ -3,15 +3,15 @@ import Display.py
 import tempScript.py
 import time
 
-disp = On_Display() #from the display script
-cur_temp = read_temp() #from the temp script
+disp = Display.On_Display() #from the display script
+cur_temp = tempScript.read_temp() #from the temp script
 cur_device = "none"
 
-while(true): #create an infinite loop, the idea should be to have this run for the duration of the device uptime
+while True: #create an infinite loop, the idea should be to have this run for the duration of the device uptime
     #fetch the current temp
-    temp = read_temp()
+    temp = tempScript.read_temp()
     time.sleep(.5)
-    cur_temp = temp
+    cur_temp = tempScript.read_temp()
     #TODO: insert a line here to get the current connected device name
     if temp != cur_temp: #TODO: include an OR here for if the connected device changes
         disp.draw_labels(temp, cur_device)
