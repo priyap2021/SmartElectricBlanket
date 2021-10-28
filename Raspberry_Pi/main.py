b8eca86121ee8b66af885cb2662e1ffb545ee13e
MMercer8989 <46968747+MMercer8989@users.noreply.github.com>
2021-10-27 21:06:23 -0400

    Configure 'main.py' to Properly Check for Temperature Changes
    After looking at what I had previously written, I realized that it would be necessary to include a wait period before checking the temperature again. Additionally, I corrected a mistake that I had made earlier where 'cur_temp' was not being updated.

--- Raspberry_Pi/main.py
+++ Raspberry_Pi/main.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 import Display.py
 import tempScript.py
+import time
 
-disp = On_Display()
-cur_temp = read_temp()
+disp = On_Display() #from the display script
+cur_temp = read_temp() #from the temp script
 cur_device = "none"
 
 while(true): #create an infinite loop, the idea should be to have this run for the duration of the device uptime
     #fetch the current temp
     temp = read_temp()
+    time.sleep(.5)
+    cur_temp = temp
     #TODO: insert a line here to get the current connected device name
     if temp != cur_temp: #TODO: include an OR here for if the connected device changes
         disp.draw_labels(temp, cur_device)
